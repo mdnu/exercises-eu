2384964f12bbfddf3fc3e037ea6cf3ef52827ca5
mdnu <mark@sdentertainment.ca>
2016-09-05 19:06:37 -0400

    Code cleaning.

--- src/Problem07.java
+++ src/Problem07.java
@@ -8,47 +8,41 @@
 public class Problem07 {
 	
   public static boolean isPrime(int n) {
-    if (n == 1) {
-      return false;
-    } else {
-      if (n < 4) {
-        return true;
-      } else {
-        if (n%2 ==0) {
-          return false;
-        } else {
-          if (n < 9) {
-            return true;
-          } else {
-            if (n%3 ==0) {
-              return false;
-            } else {
-              double r = Math.floor(Math.sqrt(n));
-              double f = 5;
-			  
-              while (f<=r) {
-                if (n%f == 0) {
-                  return false;
-                } else {
-                  if (n%(f+2) ==0) {
-                    return false;
-                  }
-                }
-				
-                f+=6;
-              }
-			  
-              return true;
-            }
-          }
-        }
-      }
-    }
+	  if (n == 1) {
+		  return false;
+	  } else {
+		  if (n < 4) {
+			  return true;
+		  } else {
+			  if (n < 9) {
+				  return true;
+			  } else {
+				  if (n % 3 == 0) {
+					  return false;
+				  } else {
+					  double r = Math.floor(Math.sqrt(n));
+					  double f = 5;
+					  
+					  while (f <= r) {
+						  if (n % f == 0) {
+							  return false;
+						  } else {
+							  if (n % (f + 2) == 0) {
+								  return false;
+							  }
+						  }
+						  f += 6;
+					  }
+					  return true;
+				  }
+			  }
+		  }
+	  }
   }
   
   public static int limitnum (int a) {
-	 int limit = a;
-	 return limit;
+	  int limit = a;
+	  return limit;
   }
   
   public static int primeDivCount (int limitnum) {
@@ -59,26 +53,23 @@ public class Problem07 {
 			  count++;
 		  }
 	  }
-
 	  return count;
   }
   
   public static void main(String[] args) {
-	int limit = limitnum(10);
-    int count = 1, grand = 1;
-	
-    while (grand <= limit) {
-      if (isPrime(count) == true) {
-        if (grand == limit) {
-          System.out.println(count);
-        }
-		
-        count++;
-        grand++;
-      } else {
-        count++;
-      }
-    }
-	
+	  int limit = limitnum(10);
+	  int count = 1, grand = 1;
+	  
+	  while (grand <= limit) {
+		  if (isPrime(count) == true) {
+			  if (grand == limit) {
+				  System.out.println(count);
+			  }
+			  count++;
+			  grand++;
+		  } else {
+			  count++;
+		  }
+	  }
   }
 }
