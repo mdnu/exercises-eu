1d0654690283320aae8b00c298e2827c1d4c86ef
mdnu <mark@sdentertainment.ca>
2016-09-06 20:26:14 -0400

    Code cleaning.

--- src/Problem09.java
+++ src/Problem09.java
@@ -1,72 +1,61 @@
-// A Pythagorean triplet is a set of three natural numbers, a < b < c,
-//for which, a^2 + b^2 = c^2. For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
-//
-// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-// Find the product abc.
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c,
+ * for which, a^2 + b^2 = c^2. For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
 
-import java.util.ArrayList;
-import java.util.List;
-
-class Problem09 {
-
-  public static int gcd(int a, int b) {
-    if (b == 0) {
-      return a;
-    } else {
-      return gcd(b,a%b);
-    }
-  }
-  
-  public static int calculate(int sm, int s2, int k, int m) {
-	   int d = s2/(k*m), n = k-m;
-	   int a = d*(m*m - n*n), b = 2*d*m*n, c = d*(m*m + n*n);
-	   int product = a*b*c;
-	   return product;
-  }
-
-  public static void main(String[] args) {
-    int s = 1000;
-	int s2 = s/2;
-    double mLimit = Math.ceil(Math.sqrt(s2));
+public class Problem09 {
+	public static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		} else {
+			return gcd(b, a % b);
+		}
+	}
 	
-    for (int m = 2; m<=mLimit;m++) {
-		
-      if (s2%m == 0) {
-        int sm = s2/m;
-		
-        while (sm%2 == 0) {
-          sm = sm/2;
-        }
-		
-        if (m%2 == 1) {
-          int k = m+2;
-		  
-          while ((k < 2*m)&&(k<=sm)) {
-            if ((sm%k == 0)&&(gcd(k,m)==1)) {
-              System.out.println(calculate(sm,s2,k,m));
-              break;
-            }
-			
-            k+=2;
-          }
-		  
-        } else if (m%2 == 0) {
-          int k = m+1;
-		  
-          while ((k < 2*m)&&(k<=sm)) {
-            if ((sm%k == 0)&&(gcd(k,m)==1)) {
-              System.out.println(calculate(sm,s2,k,m));
-              break;
-            }
-			
-            k+=2;
-          }
-		  
-        }
-		
-      }
-	  
-    }
+	public static int calculate(int sm, int s2, int k, int m) {
+		int d = s2 / (k * m), n = k - m;
+		int a = d * (m * m - n * n), b = 2 * d * m * n, c = d * (m * m + n * n);
+		int product = a * b * c;
+		return product;
+	}
 	
-  }
+	public static void main(String[] args) {
+		int s = 1000;
+		int s2 = s/2;
+		double mLimit = Math.ceil(Math.sqrt(s2));
+		
+		for (int m = 2; m <= mLimit; m++) {
+			if (s2 % m == 0) {
+				int sm = s2 / m;
+				
+				while (sm % 2 == 0) {
+					sm /= 2;
+				}
+				
+				if (m % 2 == 1) {
+					int k = m + 2;
+					
+					while ((k < 2 * m) && (k <= sm)) {
+						if ((sm % k == 0) && (gcd(k,m) == 1)) {
+							System.out.println(calculate(sm, s2, k, m));
+							break;
+						}
+						k += 2;
+					}
+				} else if (m % 2 == 0) {
+					int k = m + 1;
+					
+					while ((k < 2 * m) && (k <= sm)) {
+						if ((sm % k == 0) && (gcd(k,m) == 1)) {
+							System.out.println(calculate(sm, s2, k, m));
+							break;
+						}
+						k += 2;
+					}
+				}
+			}
+		}
+	}
 }
--- src/Problem10.java
+++ src/Problem10.java
@@ -1,42 +1,39 @@
-// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-//
-// Find the sum of all the primes below two million.
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+
 
 import java.util.ArrayList;
 import java.util.List;
 
-class Problem10 {
-	
-  public static void main(String[] args) {
-	  
-    List<Long> list = new ArrayList<Long>();
-    int n = 2000000;
-    int rootn = (int) Math.sqrt(n);
-    long sum = 0;
-    boolean[] composite = new boolean[n +1];
-
-    for (int i = 2; i <= rootn; i++) {
-      if (!composite[i]) {
-        list.add(Long.valueOf(i));
+public class Problem10 {
+	public static void main(String[] args) {
+		List<Long> list = new ArrayList<Long>();
+		int n = 2000000;
+		int rootn = (int) Math.sqrt(n);
+		long sum = 0;
+		boolean[] composite = new boolean[n + 1];
 		
-        for (int k = i*i; k<= n; k += i) {
-          composite[k] = true;
-		  
-        }
+		for (int i = 2; i <= rootn; i++) {
+			if (!composite[i]) {
+				list.add(Long.valueOf(i));
+				
+				for (int k = i * i; k <= n; k += i) {
+					composite[k] = true;
+				}
+			}
+		}
 		
-      }
-    }
-
-    for (int i = rootn; i <= n; i++) {
-      if (!composite[i]) {
-        list.add(Long.valueOf(i));
-      }
-    }
-
-    for (long j:list) {
-      sum += j;
-    }
-	
-    System.out.println(sum);
-  }
+		for (int i = rootn; i <= n; i++) {
+			if (!composite[i]) {
+				list.add(Long.valueOf(i));
+			}
+		}
+		
+		for (long j : list) {
+			sum += j;
+		}
+		System.out.println(sum);
+	}
 }
